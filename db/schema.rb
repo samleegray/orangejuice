5771fcdad286fcd068409949583895cdbb1767ed
Sam Gray <samleegray@gmail.com>
2024-05-11 12:06:40 -0500

    Created new Roles scaffolding, updated scaffolding to appear correctly like other views. Not hooked into User object yet.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_11_032147) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_11_165749) do
   create_table "discussions", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -24,6 +24,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_11_032147) do
     t.datetime "updated_at", null: false
     t.integer "discussion_id", null: false
     t.index ["discussion_id"], name: "index_posts_on_discussion_id"
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
